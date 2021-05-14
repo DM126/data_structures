4eeba1b0249d8fbb16cc1af5d13b81f879a32ae1
DM126 <danny386126@gmail.com>
2021-05-14 11:25:46 -0400

    Finish tree header files

--- C++/BST/tree.hpp
+++ C++/BST/tree.hpp
@@ -3,6 +3,7 @@
 #ifndef TREE_HPP
 #define TREE_HPP
 
+#include <vector>
 #include "treenode.hpp"
 
 template <typename T>
@@ -13,6 +14,41 @@ public:
 	Tree(const Tree<T>& other);
 	~Tree();
 	Tree<T>& operator=(Tree<T> rhs); //uses copy-swap
+
+	//Adds a value to the tree.
+	//Returns false if the value already exists.
+	bool insert(const T& value);
+
+	//Returns true if the value exists in the tree
+	bool find(const T& value) const;
+
+	//Removes a specific value from the tree, returns false if the value is not present
+	bool remove(const T& value);
+
+	//Returns the value at the root of the tree without removing it.
+	//Throws underflow_error if the tree is empty.
+	T peek() const;
+
+	//Returns true if the tree contains no items
+	bool isEmpty() const;
+
+	//Returns the number of items in the tree
+	int getSize() const;
+
+	//Removes all items from the tree.
+	void clear();
+
+	//Tree traversals, returns a vector determined by traversal order
+	vector<T> inorder() const;
+	vector<T> preorder() const;
+	vector<T> postorder() const;
+
+	//Print the tree's info, used for debugging
+	void dump() const;
+
+private:
+	Node<T>* root;
+	int size;
 };
 
 #endif
--- C++/BST/treenode.hpp
+++ C++/BST/treenode.hpp
@@ -13,6 +13,31 @@ public:
 	Node(const Node<T>& other);
 	~Node();
 	Node<T>& operator=(Node<T> rhs); //uses copy-swap
+
+	//Inserts a value into one of the nodes children
+	bool insert_node(const T& value);
+
+	//Searches for a node with the given value
+	bool find_node(const T& value) const;
+
+	//Searches for and removes the node with the given value.
+	//Moves the right child node up into its place.
+	//Parent is the parent of the current node being looked at.
+	bool remove_node(Node* parent, const T& value);
+
+	//Finds the minimum value in a subtree, returns its value and removes its node.
+	T swap_minimum(Node* parent);
+
+	//Does an recursive traversal of the subtree,
+	//adding items to the vector as it goes
+	void inorder_node(vector<T>& items) const;
+	void preorder_node(vector<T>& items) const;
+	void postorder_node(vector<T>& items) const;
+
+private:
+	T data;
+	Node* left;
+	Node* right;
 };
 
 #endif
